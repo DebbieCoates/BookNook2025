af9a3d49ce6f2c7ec65c345dc2b0e68b364712ae
Debbie Stubbs <debbie.coates@hotmail.co.uk>
2025-01-13 20:37:32 +0000

    updated models table

--- booknook/settings.py
+++ booknook/settings.py
@@ -17,6 +17,12 @@ import dj_database_url
 if os.path.isfile("env.py"):
     import env
 
+
+# settings.py
+
+import os
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -143,3 +149,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
