d6be22129c22b7a4ad6ba698e1614ae3768fce5e
Debbie Stubbs <debbie.coates@hotmail.co.uk>
2025-01-13 23:07:53 +0000

    edited urls

--- book/urls.py
+++ book/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.BookList.as_view(), name='home'),
+   
     path('all_books', views.AllBooks.as_view(), name='all_books'),
+    path('', views.BookList.as_view(), name='home'),
 ]
