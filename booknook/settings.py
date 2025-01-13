5b9715b74843f2d6f3199bfab813bdb6a6c1cf18
Debbie Stubbs <debbie.coates@hotmail.co.uk>
2025-01-13 12:30:35 +0000

    Deployment commit

--- booknook/settings.py
+++ booknook/settings.py
@@ -20,6 +20,7 @@ if os.path.isfile("env.py"):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -62,6 +63,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'booknook.urls'
+
 
 TEMPLATES = [
     {
