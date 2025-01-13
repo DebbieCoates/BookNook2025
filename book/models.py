b4b58c5af9c748007e90c2ac65110853c85ac30a
Debbie Stubbs <debbie.coates@hotmail.co.uk>
2025-01-13 12:52:22 +0000

    Add installed pillow for my image imports

--- book/models.py
+++ book/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+# Define choices for the BooleanField
+YES_NO_CHOICES = [(True, 'Yes'), (False, 'No')]
+
+class Book(models.Model):
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=100)
+    synopsis = models.TextField()
+    genre = models.TextField()
+    # slug = models.SlugField(max_length=250, blank=True)  # No jls_extract_var
+    # book_cover = models.ImageField(upload_to='book_covers/', blank=True, null=True)
+    part_of_series = models.BooleanField(choices=YES_NO_CHOICES, default=False)
+    ISBN = models.CharField(max_length=20, null=True)
+
+    # Creates a slug if one is not supplied
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)  # Automatically generate the slug if not supplied
+        super(Book, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
